76c2db74edf6d5f86ad3091c1f7f39b1cfb52780
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-07-15 13:00:04 +0200

    {pop up} added

--- src/Components/topbar/OnlineNav.js
+++ src/Components/topbar/OnlineNav.js
@@ -41,13 +41,13 @@ const logout = async () =>{
                     <div className="rightNavContent">
                     <NavLink to="*" activestyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span></span>
                     </NavLink>
-                    <NavLink to="./Home"  activestyle={{color: 'black'}} exact onClick={() => {props.isMobile && props.closeMobileMenu(); window.location="/Home"}}><span><i>Home</i></span>
+                    <NavLink to="./"  activestyle={{color: 'black'}} exact onClick={() => {props.isMobile && props.closeMobileMenu(); window.location="/Home"}}><span><i>Home</i></span>
                     </NavLink>
-                    <NavLink to="./Profile" activestyle={{color: 'black'}} exact onClick={() =>{ props.isMobile && props.closeMobileMenu(); window.location="/Profile"}} ><span><i >Profile</i></span>
+                    <NavLink to="./" activestyle={{color: 'black'}} exact onClick={() =>{ props.isMobile && props.closeMobileMenu(); window.location="/Profile"}} ><span><i >Profile</i></span>
                     </NavLink>
-                    <NavLink to="./Book" activestyle={{color: 'black'}} exact onClick={() =>{ props.isMobile && props.closeMobileMenu(); window.location="/Book"}} ><span><i >Book</i></span>
+                    <NavLink to="./" activestyle={{color: 'black'}} exact onClick={() =>{ props.isMobile && props.closeMobileMenu(); window.location="/Book"}} ><span><i >Book</i></span>
                     </NavLink>
-                    <NavLink to="./Login" activestyle={{color: 'black'}} exact onClick={logout} ><span ><i>Log Out</i></span>
+                    <NavLink to="./" activestyle={{color: 'black'}} exact onClick={logout} ><span ><i>Log Out</i></span>
                     </NavLink>
                     </div>
                    
